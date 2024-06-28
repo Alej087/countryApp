9e487b830ea7a3d55342519ea74313536829d115
Alejandro González <85509092+Alej087@users.noreply.github.com>
2024-06-27 22:08:54 -0500

    created countries routing module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,18 +3,23 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
 import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
 import { HomePageComponent } from './pages/homePage/home-page.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
-import { ContactPageComponent } from './pages/contact-page/contact-page.component';
 
 @NgModule({
   declarations: [
+    AboutPageComponent,
+    ContactPageComponent,
+    HomePageComponent,
+    SidebarComponent,
+  ],
+  imports: [CommonModule, RouterModule],
+  exports: [
     HomePageComponent,
     AboutPageComponent,
     SidebarComponent,
     ContactPageComponent,
   ],
-  imports: [CommonModule, RouterModule],
-  exports: [HomePageComponent, AboutPageComponent, SidebarComponent],
 })
 export class SharedModule {}
